7294d8299cd6ed8354bf6239ecf902a3c81cab2b
Amintas <amintaspereira@hotmail.com>
2021-10-30 10:03:17 -0300

    Creating project and git repository

---
+++ src/db/DbIntegrateExpection.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbIntegrateExpection extends RuntimeException {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public DbIntegrateExpection(String msg) {
+		super(msg);
+	}
+}
